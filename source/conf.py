b60e01e1cf2f60a37f255aad13ca4193c5ff3583
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-07-15 23:10:50 +0200

    Fixed intersphinx_mapping

--- source/conf.py
+++ source/conf.py
@@ -263,4 +263,6 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'ertza': ('http://ertza.readthedocs.io/en/latest', None),
+}
